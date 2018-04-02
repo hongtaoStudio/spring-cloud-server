21676a6aad91c48d9bfd28dc2d0235a4bb2d67ab
吕洪涛 <htlv@51tniu.com>
2018-04-02 16:58:48 +0800

    完成部署

--- spring-cloud-feign/src/main/java/com/htlv/BalanceRemote.java
+++ spring-cloud-feign/src/main/java/com/htlv/BalanceRemote.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "balance-provider")
+@FeignClient(name = "balance-provider", fallback = BalanceRemoteHystrix.class)
 public interface BalanceRemote {
 
     @RequestMapping(value = "/getUserBalance/{name}", method = RequestMethod.GET)
